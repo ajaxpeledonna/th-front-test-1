b50fa61f37b745f757c3fdd18d75201a06d583d1
ajaxpeledonna <ajaxpeledonna@gmail.com>
2021-09-09 15:20:12 +0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DataBlockComponent } from './data-block.component';
+import { CardComponent } from './card.component';
+import { ListComponent } from './list.component';
+import { ButtonsComponent } from './buttons.component';
+import { AddModule } from '@carbon/icons-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataBlockComponent,
+    CardComponent,
+    ListComponent,
+    ButtonsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AddModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/data-block.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'data-block',
+  templateUrl: './data-block.component.html',
+  styleUrls: ['./data-block.component.css']
+})
+export class DataBlockComponent {}
